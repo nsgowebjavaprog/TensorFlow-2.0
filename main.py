e15e3f85ce74b974097f5dc9d364d1dbcf6a7532
ns loni <nsloni@gmail.com>
2025-04-28 18:01:46 +0530

    added

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# import tensorflow as tf
+
+# print(tf.__version__)
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  
+
 import tensorflow as tf
 
-print(tf.__version__)
+# Initialize a TensorFlow session
+
+x = tf.constant(4, shape=(), dtype=tf.float32)  # ([[1, 2, 3], [4, 5, 6]])
+print(x)
+
+y = tf.constant([[1,2,3], [4,5,6]])
+print(y)
+
+# Maths Operations
+
+
+# Indexing
+
+
+
+# Reshape
